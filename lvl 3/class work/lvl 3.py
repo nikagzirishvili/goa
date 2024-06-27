769ca04025a941d8acde735dee682b6d78a67d18
nikagzirishvili <gzirishvilinika05@gmail.com>
2024-06-27 22:10:22 +0400

    vlv 3

---
+++ lvl 3/class work/lvl 3.py
@@ -0,0 +1,7 @@
+#print( 7 + 3 )  #mimateba
+#print(7-3) #gamokleba
+#print(7 * 3) #gamravleba
+#print(21 // 3) #gayofa. / erti es chawers atwiladshi 
+#print(5 ** 5) # axarisxeba 5 * 5 * 5 * 5 * 5
+massage = "Game over"
+print (massage)
---
+++ lvl 3/home work/lvl 3.py
@@ -0,0 +1,27 @@
+name = "nika"
+last_name = "gzirishvili"
+age = 17
+hobby = "watching moves"
+country = "Georgia"
+print (name)
+print (last_name)
+print (age)
+print (hobby)
+print (country)
+print ( "i am  " + name + " " + last_name + " i'm  " + str(age) + " years old, " + " my hobby is " + hobby + " and i am from " + country )
+print ( 10 + 141 )
+print ( 141 + 1143 )
+print ( 14 + 1678 )
+print ( 123 + 123 )
+print ( 1235 - 125 )
+print (1489 - 678 )
+print (1906 - 346 )
+print ( 246 - 245)
+print ( 6 * 8 )
+print (7 * 9 )
+print ( 12 * 43 )
+print ( 54 * 12 )
+print ( 22 // 2 )
+print ( 45 // 5 )
+print ( 121 // 11 )
+print ( 1000 // 10 )
