dd89748d6a6db64b105add0dc5f9951f0a3f9592
nikagzirishvili <gzirishvilinika05@gmail.com>
2024-06-30 17:20:48 +0400

    vlv 4

---
+++ lvl 4/class work/lvl 4.py
@@ -0,0 +1,22 @@
+# price = 130
+# print (price)
+
+
+
+
+
+# 1)
+name = "Anna"
+score = 96
+print (name)
+print (score)
+flight_n = "BA0117"
+destination = "New York"
+distance = 1580
+print (flight_n)
+print (destination)
+print (distance)
+salary = 150000
+pay_raise = 10000
+new_salary = (salary + pay_raise)
+print (new_salary)
