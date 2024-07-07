6f851ca92c3d02ac09f30861c93f92ee7679b7fb
nikagzirishvili <gzirishvilinika05@gmail.com>
2024-07-07 20:33:03 +0400

    lvl5,6,7

---
+++ lvl 5/home work/lvl 5.py
@@ -0,0 +1,60 @@
+# დავალება 1)
+# x = int(input("enter x:" ))
+# y = int(input("enter y:" ))
+# n = int(input("enter n:" ))
+# m = int(input("enter m:" ))
+# print (x + y - n * m)
+# print (x ** y ** n ** m )
+# print (x - y - n - m)
+# print (x // y // n // m)
+# print (x * y * n * m)
+
+# დავალება 2)
+# age = int(input("enter your age:"))
+# print (age + 20)
+
+# დავალება 3)
+# name = "nika"
+# last_name = 'gzirishvili'
+# age = 17
+# country = 'georgia'
+# city = "tbilisi"
+# fav_country = 'norway'
+# color = "yellow"
+# fav_car = 'ford mustang'
+# fav_food = 'xinkali'
+# fav_sport = 'football'
+# fav_football_team = 'barcelona'
+# print ('my name is ' + name + ' my last name is ' + last_name + 
+# ' i am ', str(age) + ' years old ' + 'i am from ' + country + ' i am living in ' 
+# + city + ' my fav country is ' + fav_country + ' in color that i liked most is ' + color +
+# ' my fav car is ' + fav_car + ' my fav food is ' + fav_food + ' also my fav sport is ' + fav_sport + 
+# ' and my fav football team is ' + fav_football_team) 
+
+# დავალება 4)
+# name = input('enter your name: ')
+# print(name + ' გილოცავ დაბადების დღეს მრავალს დაესწარი')
+
+# დავალება 5)
+# x = int(input('enter x:'))
+# y = int(input('enter y:'))
+# print(x + y)
+
+# დავალება 6)
+# x = int(input('enter height:'))
+# y = int(input('enter width:'))
+# print(x * y)
+
+# დავალება 7)
+# x = int(input('enter x: '))
+# print (x * 2)
+
+# დავალება 8)
+x = int(input('enter height:'))
+y = int(input('enter width:'))
+print((x + y)* 2)
+
+# დავალება 9)
+# x = int(input("enter x:" ))
+# print (x ** 2)
+
