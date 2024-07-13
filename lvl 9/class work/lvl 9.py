03b53ffc189f04cf15db1c8ea34e33b055f71ada
nikagzirishvili <gzirishvilinika05@gmail.com>
2024-07-13 10:49:47 +0400

    lvl 8, lvl 9

---
+++ lvl 9/class work/lvl 9.py
@@ -0,0 +1,8 @@
+savings = int(input("enter your saving: "))
+balance = savings * 1.05
+# print (f'Ammount in 1 year: {balance}')
+print('ammount in 1 year:', balance)
+
+
+
+
---
+++ lvl 9/home work/lvl 9.py
@@ -0,0 +1,46 @@
+#დავალება3: ჩამოწერეთ ცვლადები, რომლებსაც ექნებათ სხვადასხვა მონაცემთა რიპი, შემდეგ კი დაპრინტეთ რა მონაცემთა ტიპებისა ცვლადები
+# age = int(input('enter your age: '))
+# print (type(age))
+
+# name = 'name'
+# print(type(name))
+
+# number = 1.7
+# print(type(number))
+
+# num1 = str(17)
+# print (type(num1))
+
+
+#დავალება2: შემოიტანეთ მომხმარებელს ასაკი და დაპრინტეთ რამდენი წლის იქნება 10 წლის შემდეგ
+
+# age = int(input('enter your age: '))
+# print (age + 10)
+
+
+#დავალება1: ეცადეთ input-ბით, და მათემატიკური ოპერაციებით შექმნათ კალკულატორი'
+
+# x = int(input('enter x: '))
+# y = int(input('enter y: '))
+# user = input('Say what you want the calculator to do ')
+# if user == '+' :
+#     print(x + y)
+# elif user == '-' :
+#     print(x - y)
+# elif user == '*' :
+#     print(x * y)
+# elif user == '//' :
+#     print(x // y)
+# elif user == '**' :
+#     print(x ** y)
+# else: 
+#     print('error')
+
+#დავალება: მომხმარებელს შემოატანინეთ 2 რიცხვი და გაკეტეთ მათემატიკური ოპერაციები
+# x = int(input('enter x: '))
+# y = int(input('enter y: '))
+# print (x + y)
+# print (x - y)
+# print (x / y)
+# print (x * y)
+# print (x ** y)
