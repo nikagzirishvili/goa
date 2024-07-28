f192dff41a45b0d8caf07ac5a146f6553e66f237
nikagzirishvili <gzirishvilinika05@gmail.com>
2024-07-28 15:28:34 +0400

    davalebebi

---
+++ lvl 13/class work/lvl 13.py
@@ -0,0 +1,52 @@
+# დავალება 1
+# print('კოღო ოთახშია')
+# bite = int(input('რამდენჯერ დააპირა კოღომ კბენა?: '))
+# kill_kogo = int(input('რამდენჯერ მოუქნიეთ ხელი?: '))
+# print = (bite > 2) or (kill_kogo > 2)
+
+
+
+
+# დავალება 2
+# x = int(input('enter x: '))
+# y = int(input('enter y: '))
+# print (x + y)
+# x = int(input('enter x: '))
+# y = int(input('enter y: '))
+# print (x - y)
+# x = int(input('enter x: '))
+# y = int(input('enter y: '))
+# print (x * y)
+# x = int(input('enter x: '))
+# y = int(input('enter y: '))
+# print (x // y)
+
+
+
+
+#დავალება 3
+# name = input('what is your name: ')
+# last_name = input ('what is ur last name: ')
+# age = int(input('how old are you: '))
+# country = input('where are you from: ')
+# print('my name is ' + name + ' my last name is ' + last_name + ' i am ' + str(age) + ' years old' + ' i am form ' + country)
+
+
+
+
+# დავალება 4
+# პირველი გაყოფა (/) მოცემულ პასუხს აქცევს float-ად ხოლო (//) ეს მოცემულ ფასუხს წერს integer-ის სახით
+# x = int(input('enter x: '))
+# y = int(input('enter y: '))
+# print (x / y)
+# x = int(input('enter x: '))
+# y = int(input('enter y: '))
+# print (x // y)
+
+
+
+
+#დავალება 5
+# type ფუნქცია გვეუბნება მოცემულ print ინსტრუქციაში მოცემული print-ი ინტეჯერია ფლოატი თუ სტრინგი
+print(type('nika'))
+
---
+++ lvl 13/home work/lvl 13.py
@@ -0,0 +1,22 @@
+# x = int(input('enter x: '))
+# y = int(input('enter y: '))
+# user = input('say what do you want to calculator do: ')
+# if user == '>' :
+#     print(x > y)
+# elif user == '<' :
+#     print(x < y)
+# elif user == '+' :
+#     print(x + y)
+# elif user == '-' :
+#     print(x - y)
+# elif user == '*' :
+#     print (x * y)
+# elif user == '//' :
+#     print (x // y)
+
+
+# nam1 = int(13)
+# print (nam1 < 78)\
+
+
+
