7f84ce81992e0abb96c0ee4d24b7694020bba24c
nikagzirishvili <gzirishvilinika05@gmail.com>
2024-08-11 17:59:32 +0400

    commit

---
+++ lvl 21/class work/lvl 21.py
@@ -0,0 +1,15 @@
+# for i in range(50):
+#     print('nika')
+
+
+
+
+# x=0
+# while x < 20 :
+#     x = x + 1
+#     print('goa')
+
+y =int((input('Guess the number from 1 to 100: '))) 
+while y < 5 and y < 100:
+    y =int((input('it is incorect: ')))
+print('its corect')
